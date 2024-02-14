fcf2bd1e88a87cb33ea5a41e485fc6fbba404465
ChechuNeta <127496996+ChechuNeta@users.noreply.github.com>
2024-02-14 12:51:55 +0100

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -16,3 +16,66 @@ function ocultarInstrucciones()
     document.getElementById('instrucciones').style.display = 'none';
     document.getElementById('instruccionesVolver').style.display = 'none';
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+var intervalodesaparecer;
+var target = document.getElementById("target");
+var juegoIniciado = false;
+
+function Empezarjuego(dificultad)
+{
+    document.querySelector('.contenedor').style.display = 'none';
+    
+    target.style.display = 'block';
+    reposicionar();
+    
+   
+    if (!juegoIniciado) {
+        juegoIniciado = true;
+        setTimeout(function () {
+            document.querySelector('.contenedor').style.display = 'grid';
+            target.style.display = 'none';
+            juegoIniciado = false; 
+            clearTimeout(intervalodesaparecer);
+        }, 20000); 
+    }
+    
+}
+
+function reposicionar() 
+{
+    let posicionx = Math.round(Math.random() * 95);
+    let posiciony = Math.round(Math.random() * 95);
+    let tamaño = randomIntFromInterval(12, 40);
+
+    // Mostrar el target y establecer su posición y tamaño
+    target.style.display = 'block';
+    target.style.left = posicionx + "%";
+    target.style.top = posiciony + "%";
+    target.style.width = tamaño + "px";
+    target.style.height = tamaño + "px";
+
+    // Limpiar el temporizador anterior (si existe)
+    clearTimeout(intervalodesaparecer);
+
+    // Configurar el nuevo temporizador para reposicionar el target después de 2 segundos si no es pulsado
+    intervalodesaparecer = setTimeout(function () {
+        reposicionar();
+    }, 2000);
+   
+}
+function randomIntFromInterval(min,max)
+{
+    return Math.floor( Math.random()*  ( max - min + 1 ) + min );
+}
+
+target.onmousedown = function() 
+{
+    targetClick();
+}
+
+function targetClick()
+{
+        reposicionar();   
+}
