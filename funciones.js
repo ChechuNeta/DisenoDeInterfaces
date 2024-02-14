cc53a4931155ba75b1617e2681b39089f010a8de
ChechuNeta <127496996+ChechuNeta@users.noreply.github.com>
2024-02-14 10:32:19 +0100

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -1,15 +1,18 @@
-function mostrarInstrucciones() {
-    // Oculta el contenedor principal
-    document.querySelector('.opciones').style.display = 'none';
+function mostrarInstrucciones() 
+{
+    document.querySelectorAll('.contenedor button, .contenedor select').forEach(element => {
+        element.style.display = 'none';});
 
-    // Muestra el contenedor de instrucciones
     document.getElementById('instrucciones').style.display = 'block';
+    document.getElementById('instruccionesVolver').style.display = 'block';
 }
 
-function ocultarInstrucciones() {
-    // Muestra el contenedor principal
-    document.querySelector('.contenedor').style.display = 'grid';
+function ocultarInstrucciones() 
+{
+    document.querySelector('.contenedor h1').style.display = 'block';
+    document.querySelectorAll('.contenedor button, .contenedor select').forEach(element => {
+        element.style.display = 'block';});
 
-    // Oculta el contenedor de instrucciones
     document.getElementById('instrucciones').style.display = 'none';
+    document.getElementById('instruccionesVolver').style.display = 'none';
 }
